2df89259fccfc24db1c184da39d44d1b025cca52
Franklimello <frank.melo.wal@gmail.com>
2025-07-02 15:54:32 -0300

    colaboradores cor azul

--- app/_components/colaboradores.tsx
+++ app/_components/colaboradores.tsx
@@ -1,36 +1,36 @@
 import Image from 'next/image'
 import franlim from "../../public/franklim.jpeg"
 import fernandinho from "../../public/fernando.jpeg"
+import edimar from "../../public/edimar.jpeg"
 
 const colaboradores = [
     {nome:"Franklim", image:franlim},
     {nome:"Fernandinho", image:fernandinho},
-    
+    {nome:"Edimar", image:edimar},
+       
 ]
 
 export function Colaboradores() {
     return (
-        <section className='py-8 text-white flex bg-indigo-900'>
+        <section className='py-8  font-bold flex bg-[#FDF6ec] '>
             <div className='container mx-auto px-4 '>
                 <div>
-                    <h1 className='text-3xl font-semibold mb-8 text-center'>
+                    <h1 className='text-3xl font-semibold mb-8 text-center text-black'>
                         Colaboradores</h1>
                 </div>
-            <div className='flex flex-wrap gap-8 justify-start items-center'>
+            <div className='grid grid-cols-2 sm:grid-cols-3 gap-6 md:grid-cols-4 md:gap-2 lg:grid-cols-6 2xl:grid-cols-8 lg:gap-2 xl:gap-2 	2xl:gap-2'> 
                 {colaboradores.map(colaborador => (
-                    <div key={colaborador.nome}>
+                    <div key={colaborador.nome} className='bg-blue-900 py-4 px-4 rounded-lg flex flex-col items-center justify-center text-center 
+                        w-full sm:w-11/12 md:w-4/5  lg:w-11/12  xl:w-5/6  mx-auto'>
                         <h2>{colaborador.nome}</h2>
                         <Image 
                         src={colaborador.image} 
                         alt={colaborador.nome} 
-                        width={50}
-                        height={50}
+                        width={80}
+                        height={80}
                         quality={100}
-                        style={{
-                            width: "auto",
-                            height: "auto",
-                        }}
-                        className="object-contain rounded-[100%]"/>
+                        
+                        className="object-contain rounded-lg"/>
                     </div>
                 ))}
             </div>
